21b859eee7a0deee2618783ca1626c89553242c5
aspirisen <aspirisen@gmail.com>
2017-10-23 16:08:02 +0300

    Fix State story

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,6 +4,7 @@ const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/w
 module.exports = function(config, env) {
     const result = genDefaultConfig(config, env);
 
+    result.devtool = webpackConfig.devtool;
     result.module.rules = result.module.rules.concat(webpackConfig.module.rules);
     result.resolve.extensions = result.resolve.extensions.concat(webpackConfig.resolve.extensions);
     result.plugins = result.plugins.concat(webpackConfig.plugins);
--- stories/State.tsx
+++ stories/State.tsx
@@ -1,11 +1,10 @@
-import * as React from "react";
-import { Lens, StatefulLens } from "../";
+import * as React from 'react';
+import { Lens, StatefulLens } from '../';
 
 export class State extends React.Component {
-
     public state = {
-        name: "Dropdown one",
-        turn: "turn",
+        name: 'Dropdown one',
+        turn: 'turn',
     };
 
     private lens = new StatefulLens(
@@ -13,24 +12,26 @@ export class State extends React.Component {
         {
             name: {
                 opened: false,
-                items: ["Dropdown one", "Dropdown two", "Dropdown three"],
+                items: ['Dropdown one', 'Dropdown two', 'Dropdown three'],
             },
             turn: {
                 index: 0,
             },
         },
-        () => this.forceUpdate(),
+        () => this.forceUpdate()
     );
 
     public render() {
-        const turnLens = this.lens.prop("turn").turn(
-            (v) => ({ value: `${v.value} ${v.state.index}`, state: v.state }),
-            (v) => ({ value: v.value, state: { index: v.state.index + 2 } }),
-        );
+        const turnLens = this.lens
+            .prop('turn')
+            .turn(
+                (v) => ({ value: `${v.value} ${v.state.index}`, state: v.state }),
+                (v) => ({ value: v.value, state: { index: v.state.index + 2 } }),
+            );
 
         return (
             <div>
-                <Dropdown lens={this.lens.prop("name")} />
+                <Dropdown lens={this.lens.prop('name')} />
                 <Turn lens={turnLens} />
             </div>
         );
@@ -41,21 +42,21 @@ const Turn = (props: { lens: Lens<any> }) => (
     <div>
         <input value={props.lens.get()} onChange={(e: any) => props.lens.set(e.target.value)} />
 
-        <button onClick={() => props.lens.state.prop("index").set(props.lens.state.get().index + 1)}>
+        <button onClick={() => props.lens.state.prop('index').set(props.lens.state.get().index + 1)}>
             Increase index in state: {props.lens.state.get().index}
         </button>
     </div>
 );
 
 const Dropdown = (props: { lens: Lens<any> }) => {
-    const opened = props.lens.state.prop("opened").get();
+    const opened = props.lens.state.prop('opened').get();
 
     return (
         <div>
-            <div onClick={() => props.lens.state.prop("opened").set(!opened)}>{props.lens.get()}</div>
+            <div onClick={() => props.lens.state.prop('opened').set(!opened)}>{props.lens.get()}</div>
             {opened && (
                 <div>
-                    {props.lens.state.array("items").map((e: any) => (
+                    {props.lens.state.array('items').map(({}, {}, {}, e: any) => (
                         <div key={e} onClick={() => props.lens.set(e)}>
                             {e}
                         </div>
