d7589fda6d3ec20928ec93ce90b59e36a4d0db8a
Dmitry Samsonov <user1736@yandex.ru>
2017-10-24 12:54:59 +0300

    Removed outdated story

--- stories/index.tsx
+++ stories/index.tsx
@@ -1,12 +1,10 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import { App } from "./App";
-import { Polygon } from "./Polygon";
 import { State } from "./State";
 import { FormPlaygound } from "./Form";
 
 storiesOf("Button", module)
   .add("App", () => <App />)
   .add("Form", () => <FormPlaygound />)
-  .add("State", () => <State />)
-  .add("Polygon", () => <Polygon />);
+  .add("State", () => <State />);
